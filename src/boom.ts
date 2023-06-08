fc22868660143645c064270a7c6bf78ef25ea0d7
Nico Kimmel <mail@nicokimmel.de>
2023-06-08 19:51:43 +0200

    Added demo spotify api implementation

--- src/boom.ts
+++ src/boom.ts
@@ -1,8 +1,22 @@
-import express from 'express';
-import path from 'path';
+import 'dotenv/config';
+import express from "express";
+import path from "path";
+import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 
 const app = express();
 const port = 3000;
+
+function init() {
+	console.log(process.env.SPOTIFY_CLIENT_ID);
+	const spotify = SpotifyApi.withClientCredentials(process.env.SPOTIFY_CLIENT_ID!, process.env.SPOTIFY_CLIENT_SECRET!);
+	spotify.search("Flamingo", ["track"])
+	.then((items) => {
+		console.log(items.tracks.items[0]);
+	})
+    .catch((error) => {
+     	console.log(error);
+    });
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,5 +26,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
+	init();
 	console.log(`Server läuft auf http://localhost:${port}`);
 });
