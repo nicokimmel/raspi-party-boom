d5626cd3536cb1ce319bfc5bfe4c7107f7843f91
Nico Kimmel <mail@nicokimmel.de>
2023-06-09 12:03:04 +0200

    Added jquery to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt) {
                     {cwd: "src/", expand: true, src: ["**/*"], dest: "dist/"},
 					{cwd: "node_modules/bootstrap/dist/css/", expand: true, src: "bootstrap.min.css", dest: "dist/public/css/"},
 					{cwd: "node_modules/bootstrap/dist/js/", expand: true, src: "bootstrap.min.js", dest: "dist/public/js/"},
+                    {cwd: "node_modules/jquery/dist/", expand: true, src: "jquery.min.js", dest: "dist/public/js/"},
 				]
 			}
 		}
