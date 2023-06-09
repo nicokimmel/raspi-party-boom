72ae5f3c475da62a08c4804d76193e50ce2dc93f
Nico Kimmel <mail@nicokimmel.de>
2023-06-09 17:02:32 +0200

    Added moment lib; Added jquery mobile; Moved client to local JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function (grunt) {
 					{ cwd: "node_modules/bootstrap/dist/css/", expand: true, src: "bootstrap.min.css", dest: "dist/public/css/" },
 					{ cwd: "node_modules/bootstrap/dist/js/", expand: true, src: "bootstrap.min.js", dest: "dist/public/js/" },
 					{ cwd: "node_modules/jquery/dist/", expand: true, src: "jquery.min.js", dest: "dist/public/js/" },
+					{ cwd: "node_modules/moment/dist/", expand: true, src: "moment.js", dest: "dist/public/js/" }
 				]
 			}
 		},
