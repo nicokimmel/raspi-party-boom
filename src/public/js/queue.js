0158bf8c66666cefa50be74d5973948e627fa1e4
larsmager <108813515+magerlars@users.noreply.github.com>
2023-06-11 00:56:49 +0200

    refactoring; added new classes

--- src/public/js/queue.js
+++ src/public/js/queue.js
@@ -138,3 +138,48 @@ function songArraysEqual(a, b) {
     }
     return true
 }
+
+function addEntryToList(song, index) {
+
+    $("ul#searchResultList").append(`<li class="border rounded list-group-item m-0 mt-2 p-0">
+        <div class="m-0">
+            <div class="row g-0">
+                <div class="col-3">
+                <img src="` + song.image + `" style="width: 100%; height: 100%" class="rounded-start" onerror='this.src="img/default.jpg"'>
+                </div>
+                <div class="col-7">
+                <div class="m-2">
+                    <div class="card-text">
+                    <div class="text-truncate">
+                    ` + song.title + ` 
+                        <br>
+                        <small class="text-body-secondary">` + song.artist + `</small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="col-2">
+                <button type="button" class="btn addToQueueButton" data-bs-dismiss="offcanvas" style="width: 100%; height: 100%"><i class="bi bi-arrow-right"></i></button>
+                <input type="hidden" value="${index}" class="index"></input>
+                </div>
+            </div>
+        </div>
+    </li>`)
+
+}
+
+function deleteCurrentEntries() {
+
+    $("ul#searchResultList").empty()
+
+}
+
+$('#Searchbutton').on('click', function (e) {
+    console.log("Focus")
+    setTimeout(function () {
+        $('#searchTextInput').focus()
+    }, 500)
+})
+
+
+
