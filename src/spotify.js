7a5e57c5573f1ffdb2643f0c8b1cf4d6f3c2d746
Nico Kimmel <mail@nicokimmel.de>
2023-06-11 00:29:35 +0200

    Changes 2

--- src/spotify.js
+++ src/spotify.js
@@ -61,7 +61,7 @@ class SpotifyWrapper {
 
                 setTimeout(() => {
                     self.refreshAccessToken()
-                }, data.body['expires_in'] * 1000)
+                }, (data.body['expires_in'] - 1) * 1000)
 
                 callback()
             },
