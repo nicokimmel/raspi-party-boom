b9d6ec98264a0e9aa477caf04a8c934b27e12d57
Nico Kimmel <mail@nicokimmel.de>
2023-06-11 13:03:58 +0200

    Changes

--- src/shell.js
+++ src/shell.js
@@ -31,20 +31,20 @@ class ShellWrapper {
         return [
             ["00:11:22:33:44:55", "Alice's Laptop", 2],
             ["AA:BB:CC:DD:EE:FF", "Bob's Smartphone", 2],
-            ["11:22:33:44:55:66", "Charlie's Tablet", 2],
+            ["11:22:33:44:55:66", "Charlie's Tablet", 1],
             ["FF:EE:DD:CC:BB:AA", "David's Desktop", 2],
-            ["12:34:56:78:90:AB", "Emma's iPhone", 2],
+            ["12:34:56:78:90:AB", "Emma's iPhone", 3],
             ["98:76:54:32:10:FE", "Frank's Printer", 2],
-            ["AB:CD:EF:12:34:56", "Grace's Router", 2]
+            ["AB:CD:EF:12:34:56", "Grace's Router", 3]
           ];
     }
 
     getBlockedClients() {
         //#deny_mac_file=/etc/hostapd.deny
         return [
-            ["01:23:45:67:89:0A", "Henry's Smart TV", 2],
-            ["23:45:67:89:0A:BC", "Isabella's Smartwatch", 2],
-            ["CD:EF:12:34:56:78", "Jack's Gaming Console", 2]
+            ["01:23:45:67:89:0A", "Henry's Smart TV", 4],
+            ["23:45:67:89:0A:BC", "Isabella's Smartwatch", 4],
+            ["CD:EF:12:34:56:78", "Jack's Gaming Console", 4]
           ];
     }
 }
