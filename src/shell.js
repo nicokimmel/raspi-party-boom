ab7e4263b37592a0d415b262bf08ada8b6092f5f
Nico Kimmel <mail@nicokimmel.de>
2023-06-13 20:00:19 +0200

    Added implementation for blocked clients in shell wrapper

--- src/shell.js
+++ src/shell.js
@@ -1,23 +1,29 @@
-const { exec } = require("child_process");
+const fs = require("fs")
+const { exec } = require("child_process")
+
+var HOSTAPD_CONF = "/etc/hostapd/hostapd.conf"
+var HOSTAPD_DENY = "/home/nicokimmel/hostapd.deny" //"/etc/hostapd/hostapd.deny"
 
 class ShellWrapper {
 
     restartSpotifyd() {
         exec("systemctl --user restart spotifyd.service", (error, stdout, stderr) => {
             console.log(stdout)
+            console.log("[SHELL] Restarted spotifyd service")
         })
     }
 
-    setHostapd(ssid, passphrase) {        
-        exec(`sudo sed -i -s "s/^ssid=.*/ssid=${ssid}/" /etc/hostapd/hostapd.conf`, (error, stdout, stderr) => {
+    setHostapd(ssid, passphrase) {
+        exec(`sudo sed -i -s "s/^ssid=.*/ssid=${ssid}/" ${HOSTAPD_CONF}`, (error, stdout, stderr) => {
             console.log(stderr)
 
-            exec(`sudo sed -i -s "s/^wpa_passphrase=.*/wpa_passphrase=${passphrase}/" /etc/hostapd/hostapd.conf`, (error, stdout, stderr) => {
+            exec(`sudo sed -i -s "s/^wpa_passphrase=.*/wpa_passphrase=${passphrase}/" ${HOSTAPD_CONF}`, (error, stdout, stderr) => {
                 console.log(stderr)
+                console.log("[SHELL] Set new SSID " + ssid + " and new passphrase " + passphrase)
 
                 exec(`sudo systemctl --user restart hostapd`, (error, stdout, stderr) => {
                     console.log(stderr)
-                    console.log("[SHELL] Set new SSID " + ssid + " and new passphrase " + passphrase)
+                    console.log("[SHELL] Restarted hostapd service")
                 })
             })
         })
@@ -36,12 +42,22 @@ class ShellWrapper {
     }
 
     getBlockedClients() {
-        //#deny_mac_file=/etc/hostapd.deny
-        return [
-            ["01:23:45:67:89:0A", "Henry's Smart TV", 4],
-            ["23:45:67:89:0A:BC", "Isabella's Smartwatch", 4],
-            ["CD:EF:12:34:56:78", "Jack's Gaming Console", 4]
-        ];
+        let blockedClients = []
+        const filterList = fs.readFileSync(HOSTAPD_DENY, "utf-8")
+        filterList.split(/\r?\n/).forEach(mac => {
+            blockedClients.push([mac, 4]) //Soll die ShellWrapper Klasse auch die Gruppen kennen?
+        })
+        console.log("[SHELL] Get blocked clients (" + blockedClients.length + ")")
+        return blockedClients
+    }
+
+    setBlockedClients(macList) {
+        let filterList = ""
+        macList.forEach(mac => {
+            filterList += mac + "\n"
+        })
+        fs.writeFileSync(HOSTAPD_DENY, filterList)
+        console.log("[SHELL] Set new blocked clients (" + macList.length + ")")
     }
 }
 
